fc7ab7ea9f33f6751ac8db6ad4ad445c7139d4c8
whit <armstrong.whit@gmail.com>
2010-01-28 10:58:35 -0500

    initial verison w/ templates

--- test.cpp
+++ test.cpp
@@ -17,46 +17,65 @@ using std::cout;
 using std::endl;
 using boost::math::uniform;
 typedef boost::minstd_rand base_generator_type;
+#define DEBUG
 
 const double neg_inf(-std::numeric_limits<double>::infinity());
 
-//#define debug
+inline int nrow(vec v) {
+  return v.n_rows;
+}
 
-class MCMCJumper {
- private:
-  double mean_;
-  double sd_;
-  double old_mean_;
-  double old_sd_;
+inline int ncol(vec v) {
+  return v.n_cols;
+}
+
+inline int nrow(mat m) {
+  return m.n_rows;
+}
+
+inline int ncol(mat m) {
+  return m.n_cols;
+}
+
+class MCMCJumperBase {
+protected:
   static gsl_rng* rng_source_;
- public:
-  MCMCJumper(const double mean, const double sd): mean_(mean), sd_(sd), old_mean_(0), old_sd_(0) {}
+public:
+  MCMCJumperBase() {}
   static void setupRNG(gsl_rng* rng) {
     rng_source_ = rng;
   }
-  double getSD() const { return sd_; }
-  void setSD(const double sd) { sd_ = sd; }
-  double rng() {
-#ifdef debug
-    cout << "mean:sd: " << mean_ << " " << sd_ << endl;
-#endif
-    return mean_ + gsl_ran_gaussian(rng_source_, sd_);
+};
+
+template<typename T>
+class MCMCJumper : public MCMCJumperBase {
+ private:
+  T mean_;
+  T sd_;
+  T old_mean_;
+  T old_sd_;
+  T& value_;
+  const int n_;
+  void drawRNG() {
+    for(int i = 0; i < n_; i++) {
+      value_[i] = mean_[i] + gsl_ran_gaussian(rng_source_, sd_[i]);
+    }
   }
-  void jump(const double new_mean, const double new_sd) {
-#ifdef debug
-    cout << "jumping" << endl;
-#endif
+ public:
+  MCMCJumper(T& value): mean_(value), sd_(ones(nrow(value) *ncol(value))), old_mean_(value), old_sd_(value), value_(value), n_(nrow(value) *ncol(value)) { sd_.fill(0.1); }
+  const T& getSD() const { return sd_; }
+  void setSD(const T& sd) { sd_ = sd; }
+  void jump() {
     old_mean_ = mean_;
     old_sd_ = sd_;
-    mean_ = new_mean;
-    sd_ = new_sd;
+    mean_ = value_;
+    //sd_ = new_sd;
+    drawRNG();
   }
   void revert() {
-#ifdef debug
-    cout << "reverting" << endl;
-#endif
     mean_ = old_mean_;
     sd_ = old_sd_;
+    drawRNG();
   }
 };
 
@@ -72,111 +91,51 @@ public:
 // can't have value_ here b/c deterministics actually don't store their value
 template<typename T>
 class MCMCSpecialized : public MCMCObject {
+protected:
+  T value_;
 public:
-  MCMCSpecialized(): MCMCObject() {}
-  // does a stub for (virtual double logp() const = 0;) need to be defined here?
-  virtual T getValue() const = 0;
-  // cannot be overloaded: virtual const T& getValue() const = 0;
+  MCMCSpecialized(const T& shape): MCMCObject(), value_(shape) {}
+  const T& exposeValue() const {
+    return value_;
+  }
 };
 
 template<typename T>
 class MCMCDeterministic : public MCMCSpecialized<T> {
 public:
+  MCMCDeterministic(const T& initial_value): MCMCSpecialized<T>(initial_value) {}
   virtual double logp() const = 0; // must return logp of children
-  virtual void jump(int current_iteration) = 0; // must jump children
-  virtual void reject() = 0; // must reject children
-  virtual T getValue() const = 0; // pass through
+  virtual void jump(int current_iteration) = 0; // must jump children, and update self
+  virtual void revert() = 0; // must revert children, and update self
 };
 
 template<typename T>
 class MCMCStochastic : public MCMCSpecialized<T> {
 protected:
   int iteration_;
-  T value_;
-  MCMCJumper jumper_;
+  MCMCJumper<T> jumper_;
 public:
-  MCMCStochastic() : iteration_(-1), jumper_(value_, 1.0) {}
-  // virtuals
-  T getValue() const { return value_; }
-  virtual double logp(const double val) const = 0;
+  MCMCStochastic(const T& shape): MCMCSpecialized<T>(shape), iteration_(-1), jumper_(MCMCSpecialized<T>::value_) {}
   virtual double logp() const = 0;
-  void next_rng() {
-    value_ = jumper_.rng();
-  }
   void jump(const int current_iteration) {
     // only jump if we hevn't already jumped yet
     if(iteration_ == current_iteration) {
       return;
     }
-    //cout << "jump" << endl;
-    // increment iterations
     ++iteration_;
-
-    // set rng mean to current value
-    // preserve sd for now
-    jumper_.jump(value_, jumper_.getSD());
-
-    // update rng of value_ based on jumped distribution
-    next_rng();
-    //cout << "new value:" << value_ << endl;
+    jumper_.jump();
   }
-  void reject() {
+  void revert() {
     jumper_.revert();
-    // update rng of value_ based on reverted distribution
-    next_rng();
   }
 };
 
 template<typename T>
 class HyperPrior : public MCMCSpecialized<T> {
-protected:
-  const T value_;
 public:
-  HyperPrior(const T& value) : MCMCSpecialized<T>(), value_(value) {};
+  HyperPrior(const T& value) : MCMCSpecialized<T>(), MCMCSpecialized<T>::value_(value) {};
   double logp() const { return static_cast<double>(0); }
-  virtual T getValue() { return value_; }
 };
-
-// template<typename T>
-// class Likelihood {
-// private:
-//   MCMCStochastic<T>& dist_;
-//   T actual_obs_;
-//   MCMCDeterministic<T>& forecaster_;
-//   size_t n_;
-
-//   // for acceptace test
-//   base_generator_type generator_;
-//   boost::uniform_real<> uni_dist_;
-//   boost::variate_generator<base_generator_type&, boost::uniform_real<> > rng_;
-
-// public:
-//   Likelihood(MCMCStochastic<T>& dist, const T& actual_obs, MCMCDeterministic<T>& forecaster): dist_(dist), actual_obs_(actual_obs), forecaster_(forecaster), generator_(42u), uni_dist_(0,1), rng_(generator_, uni_dist_), n_(actual_obs.n_elem) {}
-
-//   void sample(size_t iterations, size_t burn, size_t thin) {
-//     for(size_t i = 0; i < iterations; i++) {
-//       double logp_old = logp();
-//       forecaster_.jump(i);
-//       double logp_new = logp();
-//       if(log(rng_()) > logp_new - logp_old) {
-// 	forecaster_.reject();
-//       }
-//     }
-//   }
-
-//   double logp() {
-//     double ans(0);
-//     T fcst = forecaster_.getValue();
-//     for(size_t i = 0; i < n_; i++) {
-//       ans += dist_.logp(fcst[i]);
-//     }
-//     //cout << "obs logp: " << ans << endl;
-//     //cout << "forecaster logp: " << forecaster_.logp() << endl;
-//     ans += forecaster_.logp();
-
-//     return ans;
-//   }
-// };
 
 template<typename T>
 class Uniform : public MCMCStochastic<T> {
@@ -186,27 +145,21 @@ class Uniform : public MCMCStochastic<T> {
   boost::uniform_real<> rng_dist_;
   boost::variate_generator<base_generator_type&, boost::uniform_real<> > rng_;
  public:
-  Uniform(const double lower_bound, const double upper_bound) :
-    MCMCStochastic<T>(),
+  Uniform(const double lower_bound, const double upper_bound, const T shape): MCMCStochastic<T>(shape),
     lower_bound_(lower_bound), upper_bound_(upper_bound), rng_dist_(lower_bound_,upper_bound_), rng_(MCMCStochastic<T>::generator_, rng_dist_)
   {
-    MCMCStochastic<T>::jumper_.setSD(getSD());
-    MCMCStochastic<T>::value_= rng_();
-    cout << "initial value: " << MCMCStochastic<T>::value_ << endl;
+    cout << "shape passed in: " << endl << shape << endl;
+    cout << "initial value: " << endl << MCMCStochastic<T>::value_ << endl;
   };
   double getSD() {
     return (upper_bound_ - lower_bound_)/pow(12,0.5);
   }
-  double logp(const double value) const {
-     if(MCMCStochastic<T>::value_ < lower_bound_ || MCMCStochastic<T>::value_ > upper_bound_) {
-#ifdef debug
-       cout << "oob" << endl;
-#endif
-     }
-    return (MCMCStochastic<T>::value_ < lower_bound_ || MCMCStochastic<T>::value_ > upper_bound_) ? neg_inf : -log(upper_bound_ - lower_bound_);
-  }
   double logp() const {
-    return  logp(MCMCStochastic<T>::value_);
+    double ans(0);
+    for(int i = 0; i < nrow(MCMCStochastic<T>::value_) * ncol(MCMCStochastic<T>::value_); i++) {
+      ans += (MCMCStochastic<T>::value_[i] < lower_bound_ || MCMCStochastic<T>::value_[i] > upper_bound_) ? neg_inf : -log(upper_bound_ - lower_bound_);
+    }
+    return ans;
   }
 };
 
@@ -225,37 +178,16 @@ public:
     MCMCStochastic<T>::jumper_.setSD(getSD());
     MCMCStochastic<T>::value_= rng_();
   }
-
   double getSD() {
     return sqrt(tau_);
   }
   double logp(const double value, const double mu, const double tau) const {
-    // logistic log, reuse later: return log(tau_) - tau_ * (value - mu_) - 2.0*log(1.0 + exp(-tau_ * (value-mu_)));
     return - 0.5 * tau * pow(value-mu,2) + 0.5*log(0.5*tau/arma::math::pi());
   }
   double logp() const {
     return  logp(MCMCStochastic<T>::value_, mu_, tau_);
    }
  };
-
-class EstimatedY : public MCMCDeterministic<mat> {
-private:
-  const mat& vals_;
-  MCMCStochastic<double>& alpha_;
-  MCMCStochastic<double>& beta_;
-public:
-  EstimatedY(const mat& vals, MCMCStochastic<double>& alpha, MCMCStochastic<double>& beta): vals_(vals), alpha_(alpha), beta_(beta) {}
-
-  mat getValue() const {
-#ifdef debug
-    cout << "alpha:beta: " << alpha_.getValue() <<":"<< beta_.getValue() << endl;
-#endif
-    return alpha_.getValue() + beta_.getValue() * vals_;
-  }
-  double logp() const { return alpha_.logp() + beta_.logp(); }
-  void jump(int current_iteration) { alpha_.jump(current_iteration); beta_.jump(current_iteration); }
-  void reject() { alpha_.reject(); beta_.reject(); }
-};
 
 template<typename T>
 class LikelihoodFunctionObject {
@@ -278,47 +210,69 @@ template<typename T>
 class NormalLikelihood : public LikelihoodFunctionObject<T> {
  private:
   const double tau_;
- public:
+public:
   NormalLikelihood(const T& actual_values, MCMCDeterministic<T>& forecaster, const double tau): LikelihoodFunctionObject<T>(actual_values, forecaster), tau_(tau) {}
   double logp(const double value, const double mu, const double tau) const {
     return - 0.5 * tau * pow(value-mu,2) + 0.5*log(0.5*tau/arma::math::pi());
   }
   double logp() const {
-    T sample = LikelihoodFunctionObject<T>::forecaster_.getValue();
+    const T& sample = LikelihoodFunctionObject<T>::forecaster_.exposeValue();
     double ans(0);
     for(int i = 0; i < LikelihoodFunctionObject<T>::actual_values_.n_elem; i++) {
+      //cout << "sample[i]" << sample[i] << endl;
       ans += logp(sample[i], LikelihoodFunctionObject<T>::actual_values_[i], tau_);
     }
     ans += LikelihoodFunctionObject<T>::forecaster_.logp();
     return ans;
   }
-  void sample(size_t iterations, size_t burn, size_t thin) {
-    for(size_t i = 0; i < iterations; i++) {
+  void sample(int iterations, int burn, int thin) {
+    for(int i = 0; i < iterations; i++) {
       double logp_old = logp();
       LikelihoodFunctionObject<T>::forecaster_.jump(i);
       double logp_new = logp();
-#ifdef debug
+#ifdef DEBUG
       cout << "old, new: " << logp_old << " : " << logp_new << endl;
 #endif
       if(logp_new == neg_inf || log(LikelihoodFunctionObject<T>::rng()) > logp_new - logp_old) {
-	LikelihoodFunctionObject<T>::forecaster_.reject();
+        cout << "revert" << endl;
+	LikelihoodFunctionObject<T>::forecaster_.revert();
       }
     }
   }
 };
 
+class EstimatedY : public MCMCDeterministic<mat> {
+private:
+  mat& X_;
+  MCMCStochastic<vec>& b_;
+public:
+  EstimatedY(mat& X, MCMCStochastic<vec>& b): MCMCDeterministic<mat>(X * b.exposeValue()), X_(X), b_(b) {
+    cout << "X:" << endl << X_;
+    //cout << "b:" << endl << b_;
+    cout << "value_:" << endl << value_;
+  }
+  double logp() const { return b_.logp(); }
+  void jump(int current_iteration) {
+    b_.jump(current_iteration);
+    value_ = X_ * b_.exposeValue();
+  }
+  void revert() {
+    b_.revert();
+    value_ = X_ * b_.exposeValue();
+  }
+};
 
 // global rng generators
-gsl_rng* MCMCJumper::rng_source_ =  NULL;
+gsl_rng* MCMCJumperBase::rng_source_ =  NULL;
 base_generator_type MCMCObject::generator_;
 
 int main() {
   const gsl_rng_type* T;
   gsl_rng_env_setup();
   T = gsl_rng_default;
-  MCMCJumper::setupRNG(gsl_rng_alloc(T));
+  MCMCJumperBase::setupRNG(gsl_rng_alloc(T));
 
-  const int N = 1000;
+  const int N = 100;
   mat X(N,2);
   mat y(N,1);
 
@@ -328,17 +282,17 @@ int main() {
 
   for(int i = 0; i < N; i++) {
     y[i] = rng_();
-    X[i,1] = rng_();
-    X[i,2] = rng_();
+    X(i,0) = 1;
+    X(i,1) = rng_();
   }
   vec coefs;
   solve(coefs, X, y);
   cout << "lm coefs:" << endl;
   cout << coefs << endl;
-  Uniform<double> alpha(-1,1);
-  Uniform<double> beta(-1,1);
-  EstimatedY obs_fcst(X, alpha, beta);
+  Uniform<vec> B(-1.0,1.0, vec(2));
+  //cout << B.exposeValue() << endl;
+  EstimatedY obs_fcst(X, B);
   NormalLikelihood<mat> likelihood(y, obs_fcst, 0.0001);
-  likelihood.sample(1e2, 10, 2);
+  likelihood.sample(1e4, 10, 2);
   return 1;
 }
